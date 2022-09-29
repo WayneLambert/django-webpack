78eba0fbd8f855c239468d0a8ad056786a43c2bb
Wayne Lambert <wayne.a.lambert@gmail.com>
2022-09-29 15:43:57 +0100

    Install and use Lodash (ES)
    Addresses the following:
    
    - Installs Lodash
    - Uses contrived example to capitalize a log statement using the
      `capitalize` function from Lodash (ES)

--- webpack/assets/scripts/circle.ts
+++ webpack/assets/scripts/circle.ts
@@ -1,11 +1,13 @@
 // Calculate Area of Circle
+import { capitalize } from 'lodash-es'
+
 const btnCalculateArea = document.getElementById('btn-area-of-circle')
 btnCalculateArea.addEventListener('click', () => {
   const circle = require('../examples/circleArea.ts')
   const radiusEl = document.getElementById('radius-of-circle')
   const radius = parseInt(radiusEl.value)
   const area = circle.area(radius)
-  const result = `The area of a circle with a radius of ${radius} is ${area}`
+  const result = capitalize(`the area of a circle with a radius of ${radius} is ${area}`)
   console.log(result)
   const areaOfCircle = document.getElementById('area-of-circle')
   areaOfCircle.textContent = result
