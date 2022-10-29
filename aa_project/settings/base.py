7282ff75f5acf443f4e159baeb47ca46689c36d2
Wayne Lambert <wayne.a.lambert@gmail.com>
2022-10-30 00:26:59 +0100

    Develop prod version
    Addresses the following:
    
    - Use SQLite version instead of Postgres on RDS (to remove cost)
    - Add volume binding to persist data as an example
    - Copy webpack directory and staticfiles directory into final image
    - Tidy up final image

--- aa_project/settings/base.py
+++ aa_project/settings/base.py
@@ -70,18 +70,6 @@ TEMPLATES = [
 WSGI_APPLICATION = "aa_project.wsgi.application"
 
 
-# Database Configuration
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.getenv('DB_NAME', ''),
-        'USER': os.getenv('DB_USER', ''),
-        'PASSWORD': os.getenv('DB_PASS', ''),
-        'HOST': os.getenv('DB_HOST', ''),
-        'PORT': os.getenv('DB_PORT', ''),
-    }
-}
-
 # Password validation
 AUTH_PASSWORD_VALIDATORS = [
     {
