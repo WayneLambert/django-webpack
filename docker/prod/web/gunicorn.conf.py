769bd7377de51e5682ff7fed0917286fc0901fbb
Wayne Lambert <wayne.a.lambert@gmail.com>
2022-10-30 19:28:37 +0000

    Update config

--- docker/prod/web/gunicorn.conf.py
+++ docker/prod/web/gunicorn.conf.py
@@ -1,12 +1,10 @@
 import os
 
-from django.conf import settings
-
 
 wsgi_app = f"{os.environ['DJANGO_PROJECT_NAME']}.asgi:application"
 bind = f"{os.environ['WEB_HOST']}:{os.environ['WEB_PORT']}"
 workers = 4
-reload = True  # TODO: Remove for Prod
+# reload = True  # TODO: Remove for Prod
 worker_class = "aa_project.workers.ProjectUvicornWorker"
-loglevel = "debug"  # TODO: Remove for Prod
+# loglevel = "debug"  # TODO: Remove for Prod
 forwarded_allow_ips = '*'
