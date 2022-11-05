9224353768578b5dcc55f0fb810e89c828c1c7ad
Wayne Lambert <wayne.a.lambert@gmail.com>
2022-11-05 15:46:09 +0000

    Update project config

--- docker/prod/web/gunicorn.conf.py
+++ docker/prod/web/gunicorn.conf.py
@@ -4,7 +4,7 @@ import os
 wsgi_app = f"{os.environ['DJANGO_PROJECT_NAME']}.asgi:application"
 bind = f"{os.environ['WEB_HOST']}:{os.environ['WEB_PORT']}"
 workers = 4
-worker_class = "aa_project.workers.ProjectUvicornWorker"
+worker_class = f"{os.environ['DJANGO_PROJECT_NAME']}.workers.ProjectUvicornWorker"
 forwarded_allow_ips = '*'
 
 # Debugging aids when deploying Prod build locally
