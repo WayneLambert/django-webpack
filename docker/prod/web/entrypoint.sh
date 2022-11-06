d6e326a21d62ee8503578a1bfcef6c91a1989a80
Wayne Lambert <wayne.a.lambert@gmail.com>
2022-11-06 14:30:24 +0000

    Update config

--- docker/prod/web/entrypoint.sh
+++ docker/prod/web/entrypoint.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 
+# Apply the database migrations
+echo "Applying database migrations..."
+python manage.py migrate
+
 # Initialise the container with Gunicorn/Uvicorn config
+echo "Starting uvicorn server..."
 gunicorn --config './docker/prod/web/gunicorn.conf.py'
